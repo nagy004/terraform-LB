0f4ca18e88f91a335282b1b07d156ab30dd38991
nagy004 <74199710+nagy004@users.noreply.github.com>
2023-01-27 00:31:28 +0200

    final

--- backend-s3.tf
+++ backend-s3.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "lab3-state"
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "lab3-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
